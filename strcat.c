7ea93c018c2dd3a0666d4ef816e5c9da1ce5bf14
capo-no-baka <141492876+capo-no-baka@users.noreply.github.com>
2023-12-08 14:38:11 -0800

    strcat

---
+++ strcat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcat - concatenates two strings
+ * @dest: str1
+ * @src: str2
+ * Return: 0
+ */
+char *_strcat(char *dest, char *src)
+{
+	size_t i;
+	size_t n = strlen(src);
+	size_t s = strlen(dest);
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[s + i] = src[i];
+	}
+	dest[s + i] = '\0';
+
+	return (dest);
+}
