bf74c81e77d6966286b3ad9400ad1e74b5da63d3
capo-no-baka <141492876+capo-no-baka@users.noreply.github.com>
2023-12-08 14:36:09 -0800

    strlen.c

---
+++ strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen - Entry point
+ * @s: input
+ * Return: 0
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+	{
+		len++;
+		s++;
+	}
+	return (len);
+}
